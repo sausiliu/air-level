5ffb54bfab11ae223267434d604f7cebb1b92277
Bird <sausiliu@163.com>
2019-03-02 23:22:57 +0800

    add schedule function

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ import ssl
 from urllib import request
 from bs4 import BeautifulSoup
 import time
+import schedule
 
 url = "http://www.air-level.com/"
-
 context = ssl._create_unverified_context()
-if __name__ == "__main__":
+
+def download_img():
     response = request.urlopen(url, context=context)
     html = response.read().decode('utf-8')
     soup = BeautifulSoup(html, 'lxml')
@@ -17,14 +18,22 @@ if __name__ == "__main__":
     img_src = img_src.attrs['src']
     print(img_src)
     imgName = 0
+
     # f = open('D:\\Temp\\' + str(imgName) + ".jpg", 'wb')
-    while 1 :
-        update_time = text_center.find('h4').get_text()
-        print(update_time)
-        try:
-            f = open('../img/' + update_time + ".png", 'wb')
-            f.write((request.urlopen(img_src)).read())
-            f.close()
-        except Exception as e:
-            print('download img error')
+    update_time = text_center.find('h4').get_text()
+    print(update_time)
+    try:
+        f = open('../img/' + update_time + ".png", 'wb')
+        f.write((request.urlopen(img_src)).read())
+        f.close()
+    except Exception as e:
+        print('download img error')
+    time.sleep(1)
+
+schedule.every().hour.do(download_img)
+
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
         time.sleep(1)
+
